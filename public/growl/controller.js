5c165046a9404c44064a94da6748a35b5279d76f
Rashmi Vijaya Prasad <rprasad@rprasad-mbp.local>
2016-02-08 12:07:27 -0800

    adding growl

---
+++ public/growl/controller.js
@@ -0,0 +1,4 @@
+var app= angular.module('myApp', ['ngAnimate', 'growlNotifications']);
+app.controller('myCtrl', function($scope) {
+    $scope.songs=['Sgt. Peppers Lonely Hearts Club Band','With a Little Help from My Friends','Lucy in the Sky with Diamonds','Getting Better' ,'Fixing a Hole','Shes Leaving Home','Being for the Benefit of Mr. Kite!' ,'Within You Without You','When Im Sixty-Four','Lovely Rita','Good Morning Good Morning','Sgt. Peppers Lonely Hearts Club Band (Reprise)','A Day in the Life'];
+  });
